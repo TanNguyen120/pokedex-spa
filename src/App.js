4b4e654ca5e4ef1808ee888c051b8710cca1716a
Taan <tannguyen1220@gmail.com>
2022-09-04 11:14:53 +0700

    plain

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './component/navBar';
+import SearchResult from './component/searchResult';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <SearchResult />
+    </div>
+  );
+}
+
+export default App;
