7f3ad9dd4b3febb693682bc655778c28fc32c949
Taan <tannguyen1220@gmail.com>
2022-10-11 11:34:32 +0700

    fea: search by name

--- src/component/body/searchBar/searchSuggestion.js
+++ src/component/body/searchBar/searchSuggestion.js
@@ -11,23 +11,26 @@ const SearchSuggestion = ({ cache, searchInput }) => {
     useEffect(() => {
         if (cache.length > 0 && searchInput.text.length >= 2) {
             // check if user already search the right name
+            if (cache.find(element => element.name === searchInput.text) === undefined) {
 
-            // filter the cache with element that have name includes the search input
-            const suggestionResult = cache.filter(pokemon => pokemon.name.includes(searchInput.text));
-            setSuggestion(suggestionResult)
+                // filter the cache with element that have name includes the search input
+                const suggestionResult = cache.filter(pokemon => pokemon.name.includes(searchInput.text));
+                setSuggestion(suggestionResult)
+            }
         } else {
             setSuggestion([]);
         }
     }, [searchInput]);
     return (
-        <div className='bg-slate-200 font-mono text-base mx-16 grid grid-cols-1'>
+        <div className='bg-white font-mono text-base mx-16 grid grid-cols-1 rounded-sm divide-y divide-neutral-400'>
             {
                 suggestion.map(
                     element => <div
                         key={element.name}
-                        className=" hover:bg-slate-400 hover:cursor-pointer text-left font-medium"
+                        className=" hover:bg-slate-400 hover:cursor-pointer text-left font-medium m-2 p-2"
                         onClick={e => {
                             dispatch(findPokeByName(element.name));
+                            dispatch(setInput(element.name))
                             setSuggestion([]);
                         }}
                     >
