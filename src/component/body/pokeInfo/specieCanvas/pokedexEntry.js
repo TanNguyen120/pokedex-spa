d82904022c7e83031015e39ef34b7a5ca60c8da5
Taan <tannguyen1220@gmail.com>
2022-10-28 06:05:47 +0700

    fea: multi language pokedex entries

--- src/component/body/pokeInfo/specieCanvas/pokedexEntry.js
+++ src/component/body/pokeInfo/specieCanvas/pokedexEntry.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 import PokedexDescription from './pokedexDescription';
 
 
 const PokedexEntry = ({ pokedexEntry }) => {
+    const webLanguage = useSelector(state => state.webSettings.language);
 
-    // get all english description of pokedex entry
-    const engEntry = [];
-    pokedexEntry.forEach(element => {
-        if (element.language.name === 'en') {
-            engEntry.push(element);
+    const [languageEntries, setLanguageEntries] = useState(null);
+
+    useEffect(() => {
+        const getPokedexEntriesBaseOnLanguage = async (pokedexEntry, language) => {
+            // get all description of pokedex entry in webLanguage
+            const langEntry = [];
+            await pokedexEntry.forEach(element => {
+                if (element.language.name === language) {
+                    langEntry.push(element);
+                }
+            });
+            setLanguageEntries(langEntry);
         }
-    });
+        getPokedexEntriesBaseOnLanguage(pokedexEntry, webLanguage);
+    }, [webLanguage, pokedexEntry])
+
     return (
         <div>
-            <PokedexDescription pokeEntry={engEntry} />
+            {languageEntries && <PokedexDescription pokeEntry={languageEntries} />}
         </div>
     )
 }
