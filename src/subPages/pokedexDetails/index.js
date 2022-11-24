aaa7cc6845133f96f99a7e9c340742dd51c9f7f0
Taan <tannguyen1220@gmail.com>
2022-11-24 08:11:14 +0700

    Preapare some display info

--- src/subPages/pokedexDetails/index.js
+++ src/subPages/pokedexDetails/index.js
@@ -1,7 +1,10 @@
 // in react router v6.4 we can define a loader function that can access to url param to call api 
 
 import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useLoaderData } from "react-router-dom";
+import LoadingSpinner from "../../component/loadingSpiner";
 
 // loader function later will be call in the router 
 const loader = async ({ params }) => {
@@ -11,12 +14,29 @@ const loader = async ({ params }) => {
 }
 const PokeDexDetails = () => {
     const pokeDexDetails = useLoaderData();
+    const webLanguage = useSelector(state => state.webSettings.language);
+    const [descriptionsLan, setDescription] = useState(null);
+
+    useEffect(() => {
+        const setDisplayData = async () => {
+            let result = "";
+            await pokeDexDetails.descriptions.forEach(element => {
+                if (element.language.name === webLanguage) {
+                    result = element;
+                }
+            });
+            setDescription(result)
+        }
+        setDisplayData();
+    }, [pokeDexDetails, webLanguage])
     return (
         <div className=' bg-smallGreyWhite bg-repeat min-h-screen font-serif'>
             <div className='md:container md:mx-auto'>
                 <div className='grid grid-cols-1'>
                     <div className=' rounded-xl p-3 bg-slate-200 border border-indigo-800  m-10'>
-                        {JSON.stringify(pokeDexDetails)}
+                        <div className=" mx-8 my-3 text-lg font-semibold ">
+                            {descriptionsLan ? descriptionsLan.description : <LoadingSpinner />}
+                        </div>
                     </div>
                 </div>
             </div>
