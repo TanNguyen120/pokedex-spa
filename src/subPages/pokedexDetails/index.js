70477b2052e9da69029b67f11901d317f869a250
Taan <tannguyen1220@gmail.com>
2022-11-25 07:58:54 +0700

    some small info in pokedex page

--- src/subPages/pokedexDetails/index.js
+++ src/subPages/pokedexDetails/index.js
@@ -4,7 +4,9 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useLoaderData } from "react-router-dom";
+import InfoSmallComponent from "../../component/body/pokeInfo/infoCanvas/inforSmallComponent";
 import LoadingSpinner from "../../component/loadingSpiner";
+import VersionGroupBtn from "./versionGroupBtn";
 
 // loader function later will be call in the router 
 const loader = async ({ params }) => {
@@ -36,6 +38,11 @@ const PokeDexDetails = () => {
                     <div className=' rounded-xl p-3 bg-slate-200 border border-indigo-800  m-10'>
                         <div className=" mx-8 my-3 text-lg font-semibold ">
                             {descriptionsLan ? descriptionsLan.description : <LoadingSpinner />}
+                        </div>
+                        <div className=" grid grid-cols-3">
+                            <InfoSmallComponent tile='Main Serries' detail_info={pokeDexDetails.is_main_series ? 'Yes' : 'No'} />
+                            <InfoSmallComponent tile='Region' detail_info={pokeDexDetails.region ? pokeDexDetails.region.name : 'None'} />
+                            <InfoSmallComponent tile='Version Group' detail_info={pokeDexDetails.version_groups.length > 0 ? pokeDexDetails.version_groups.map(e => <VersionGroupBtn versionName={e.name} />) : 'None'} />
                         </div>
                     </div>
                 </div>
---
+++ src/subPages/pokedexDetails/versionGroupBtn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const VersionGroupBtn = ({ versionName }) => {
+    return (
+        <div>
+            <Link to={`/t-pokedex/version_group/${versionName}`} className=' hover:cursor-pointer hover:bg-slate-200 '>{versionName}</Link>
+        </div>
+    )
+}
+
+export default VersionGroupBtn
