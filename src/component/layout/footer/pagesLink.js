46193a4813a8f6bf4572d40a4b4a4ca51e2374b5
Tan Nguyen <tannguyen1220@gmail.com>
2022-12-07 10:25:17 +0700

    items list nav bar

--- src/component/layout/footer/pagesLink.js
+++ src/component/layout/footer/pagesLink.js
@@ -10,7 +10,7 @@ const SingleLink = ({ name, url }) => {
 }
 
 
-
+// quick link to pages
 const PagesLink = () => {
     return (
         <div className=' grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 text-left'>
@@ -25,7 +25,6 @@ const PagesLink = () => {
                 <SingleLink name='Contest' url={'/t-pokedex/contest'} />
                 <SingleLink name='Super Contest' url={'/t-pokedex/super-contest'} />
                 <SingleLink name='Encounter' url={'/t-pokedex/encounter'} />
-
             </div>
         </div>
     )
--- src/component/layout/navBar/gameItemsNav/gameItemsMenu.js
+++ src/component/layout/navBar/gameItemsNav/gameItemsMenu.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 const GameItemsMenu = () => {
     return (
         <>
+            <Link to="/t-pokedex/items-list">
+                <div className=' hover:bg-slate-300 hover:rounded-lg hover:text-lg hover:font-semibold p-2 '>
+                    Items List
+                </div>
+            </Link>
             <Link to="/t-pokedex/berries">
                 <div className=' hover:bg-slate-300 hover:rounded-lg hover:text-lg hover:font-semibold p-2 '>
                     Berries
-                </div>
-            </Link>
-            <Link to="/t-pokedex/pokedexs">
-                <div className=' hover:bg-slate-300 hover:rounded-lg hover:text-lg hover:font-semibold p-2 '>
-                    PokéDex
                 </div>
             </Link>
         </>
