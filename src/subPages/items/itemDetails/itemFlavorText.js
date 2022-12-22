a43c5a3f37491efa0233b2c4c86192494e180621
Taan <tannguyen1220@gmail.com>
2022-12-22 10:41:10 +0700

    styling the items flavor text

--- src/subPages/items/itemDetails/itemFlavorText.js
+++ src/subPages/items/itemDetails/itemFlavorText.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const FlavorTextContent = ({ text, versionGroup }) => {
+    const versionGroupColor = []
     return (
-        <div className=' grid grid-cols-2 divide-x'>
+        <div className=' grid grid-cols-5 m-4'>
             <div>{versionGroup}</div>
-            <div>{text}</div>
+            <div className=' col-span-4'>{text}</div>
         </div>
     )
 }
@@ -12,12 +14,15 @@ const FlavorTextContent = ({ text, versionGroup }) => {
 
 
 const ItemFlavorText = ({ flavorText }) => {
+    const webLanguage = useSelector(state => state.webSettings.language);
+    const [flavorTextLanguage, setFlavorTextLanguage] = useState(null);
+
     return (
         <div className=' rounded-lg bg-slate-200 p-4 m-3'>
-            <div>
+            <div className=' text-left pl-5 text-lg font-semibold'>
                 Flavor Text:
             </div>
-            <div className=' grid grid-cols-1'>
+            <div className=' grid grid-cols-1 tex-left p-4 rounded-lg bg-white m-4 divide-y'>
                 {
                     flavorText.map(element => <FlavorTextContent text={element.text} versionGroup={element.version_group.name} />)
                 }
