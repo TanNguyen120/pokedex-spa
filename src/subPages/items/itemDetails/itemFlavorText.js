962c8d6ec7b190b428bf872605c68d0be97e598d
Taan <tannguyen1220@gmail.com>
2022-12-23 07:12:30 +0700

    flavor text base on web language setting

--- src/subPages/items/itemDetails/itemFlavorText.js
+++ src/subPages/items/itemDetails/itemFlavorText.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 const FlavorTextContent = ({ text, versionGroup }) => {
@@ -17,6 +17,18 @@ const ItemFlavorText = ({ flavorText }) => {
     const webLanguage = useSelector(state => state.webSettings.language);
     const [flavorTextLanguage, setFlavorTextLanguage] = useState(null);
 
+    useEffect(() => {
+        const setDisplayFlavorText = async () => {
+            let result = [];
+            await flavorText.forEach(element => {
+                element.language.name === webLanguage && result.push(element)
+            });
+            setFlavorTextLanguage(result)
+        }
+
+        setDisplayFlavorText();
+    }, [webLanguage, flavorText])
+
     return (
         <div className=' rounded-lg bg-slate-200 p-4 m-3'>
             <div className=' text-left pl-5 text-lg font-semibold'>
@@ -24,7 +36,7 @@ const ItemFlavorText = ({ flavorText }) => {
             </div>
             <div className=' grid grid-cols-1 tex-left p-4 rounded-lg bg-white m-4 divide-y'>
                 {
-                    flavorText.map(element => <FlavorTextContent text={element.text} versionGroup={element.version_group.name} />)
+                    flavorTextLanguage && flavorTextLanguage.map(element => <FlavorTextContent text={element.text} versionGroup={element.version_group.name} />)
                 }
             </div>
         </div>
