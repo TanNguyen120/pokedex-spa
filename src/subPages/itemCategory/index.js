cb982788bf2f8ac9068cd8e8ddc8dc687542470c
Taan <tannguyen1220@gmail.com>
2022-12-30 05:42:59 +0700

    fix item categories request url

--- src/subPages/itemCategory/index.js
+++ src/subPages/itemCategory/index.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
 import React from 'react'
+import { useLoaderData } from 'react-router-dom';
 
 
 
 // in react router v6.4 we can define a loader function that can access to url param to call api 
 // loader function later will be call in the router 
 const loader = async () => {
-    const itemCategoryNumber = ((await (axios.get(`https://pokeapi.co/api/v2/item-Category/`))).data).count;
-    const itemCategories = (await (axios.get(`https://pokeapi.co/api/v2/item-Category/?offset=0&limit=${itemCategoryNumber}`))).data;
-
-
+    const itemCategoryNumber = ((await (axios.get(`https://pokeapi.co/api/v2/item-category/`))).data).count;
+    const itemCategories = (await (axios.get(`https://pokeapi.co/api/v2/item-category/?offset=0&limit=${itemCategoryNumber}`))).data;
     return itemCategories
 }
 
 const ItemCategory = () => {
+    const itemCategories = useLoaderData();
     return (
         <div className=' bg-smallGreyWhite bg-repeat min-h-screen font-serif'>
             <div className='md:container md:mx-auto'>
@@ -29,7 +29,7 @@ const ItemCategory = () => {
                             </div>
                             <div className=' grid md:grid-cols-4 grid-cols-2 gap-y-5 mt-8'>
                                 {
-
+                                    JSON.stringify(itemCategories)
                                 }
                             </div>
                         </div>
