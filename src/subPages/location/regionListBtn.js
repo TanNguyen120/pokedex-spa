ca9e2e71af3a1e2edc3c3c00278615e2da9e4f7e
Tan Nguyen <tannguyen1220@gmail.com>
2023-03-09 10:48:41 +0700

    region list get data

--- src/subPages/location/regionListBtn.js
+++ src/subPages/location/regionListBtn.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import LoadingSpinner from '../../component/loadingSpiner';
 
 const RegionListBtn = () => {
     const [show, setShow] = useState(false);
     const [regionList, setRegionList] = useState(null)
     useEffect(() => {
         const getRegionData = async () => {
-            const resData = axios.get('')
+            // get total number of region
+            const dataCount = (await axios.get('https://pokeapi.co/api/v2/region')).data.count;
+            const resData = (await axios.get(`https://pokeapi.co/api/v2/region?offset=0&limit=${dataCount}`)).data.results;
+            setRegionList(resData);
         }
+        getRegionData()
     }, [])
     return (
         <div class="relative inline-block text-left p-20">
@@ -22,12 +27,14 @@ const RegionListBtn = () => {
 
             <div class={` right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none  ${show === false && 'hidden'}`} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                 <div class="py-1" role="none">
-                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
-                    <form method="POST" action="#" role="none">
-                        <button type="submit" class="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
-                    </form>
+                    {
+                        regionList ?
+                            regionList.map((element, index) =>
+                                <a href={`/t-pokedex/regions/${element.name}`} className="text-gray-700 block px-4 py-2 text-sm hover:bg-slate-500 hover:text-white" role="menuitem" tabindex="-1" id="menu-item-0" key={index}>{element.name}</a>
+                            )
+
+                            : <LoadingSpinner />
+                    }
                 </div>
             </div>
         </div>
