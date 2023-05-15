7a6dc67f7ce7351dbcdb6290781949456740273a
Taan <tannguyen1220@gmail.com>
2023-05-15 07:43:51 +0700

    spicy palpark table

--- src/subPages/palparkArea/enounterTable.js
+++ src/subPages/palparkArea/enounterTable.js
@@ -32,6 +32,19 @@ const EnounterTable = ({ encounterTable }) => {
                                 {element.pokemon_species.name}
                             </Link>
                         </td>
+                        <Link
+                            preventScrollReset={false}
+                            onClick={e => { findPokemon(element.pokemon_species.name) }}
+                            className=' hover:underline hover:text-blue-400' to={`/t-pokedex/pokemon/${element.pokemon_species.name}`}
+                        >
+                            <td>
+                                <img
+                                    src={`https://img.pokemondb.net/sprites/home/normal/2x/avif/${element.pokemon_species.name}.avif`}
+                                    alt={element.pokemon_species.name}
+                                    className='object-scale-down h-12 w-12 ml-4'
+                                />
+                            </td>
+                        </Link>
                         <td className={rowClass}>{element.rate}</td>
                         <td className={rowClass}>{element.base_score}</td>
                     </tr>)
