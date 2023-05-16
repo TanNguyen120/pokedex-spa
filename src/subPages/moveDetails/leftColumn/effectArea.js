9a1800ed92f064d3b1caa960169c9383d0aaede7
Taan <tannguyen1220@gmail.com>
2023-05-16 08:34:51 +0700

    small info

--- src/subPages/moveDetails/leftColumn/effectArea.js
+++ src/subPages/moveDetails/leftColumn/effectArea.js
@@ -7,7 +7,7 @@ const OptionBtn = ({ optionName, setEffectOption, effectOption }) => {
         optionName === effectOption ? setCss(' bg-slate-400 text-white') : setCss(' bg-slate-100 text-slate-400')
     }, [effectOption, optionName])
     return (
-        <div className={' ml-5 rounded-t-lg px-3 py-1 capitalize hover:cursor-pointer hover:bg-slate-300' + css} onClick={e => { setEffectOption(optionName) }}>
+        <div className={'rounded-t-lg px-3 py-1 capitalize hover:cursor-pointer hover:bg-slate-300' + css} onClick={e => { setEffectOption(optionName) }}>
             {optionName}
         </div>
     )
@@ -20,7 +20,7 @@ const EffectArea = ({ effectObject }) => {
     const options = ['long', 'short'];
     return (
         <div className=' grid grid-cols-1 pt-5'>
-            <div className='flex flex-row'>
+            <div className='flex flex-row ml-5'>
                 {options.map((element, index) => <OptionBtn key={index} effectOption={effectOption} optionName={element} setEffectOption={setEffectOption} />)}
             </div>
             <div className=' rounded-lg bg-white border border-slate-500 p-4 '>
