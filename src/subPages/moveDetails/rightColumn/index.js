0f8b8361cc10a9b6f21d4e947587afb20edab3c4
Tan Nguyen <tannguyen1220@gmail.com>
2023-05-22 08:24:31 +0700

    meta stat section

--- src/subPages/moveDetails/rightColumn/index.js
+++ src/subPages/moveDetails/rightColumn/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FlavourText from './flavourText'
+import MetaStat from './metaStat'
 
 const RightColumn = ({ moveDetails }) => {
     return (
         <div className=' m-2 p-2 rounded-lg bg-slate-200'>
             <FlavourText flavourList={moveDetails.flavor_text_entries} />
+            <MetaStat metaStat={moveDetails.meta} />
         </div>
     )
 }
