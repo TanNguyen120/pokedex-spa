5748bef7318068139e6b47516aab22fa7ec12439
Tan Nguyen <tannguyen1220@gmail.com>
2023-05-30 07:55:49 +0700

    style display

--- src/subPages/moveCategory/categoryDetails.js
+++ src/subPages/moveCategory/categoryDetails.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import LoadingSpinner from '../../component/loadingSpiner';
+import { Link } from 'react-router-dom';
 
 const CategoryDetails = ({ categoryName }) => {
     const [categoryDetails, setCategoryDetails] = useState(null);
@@ -10,9 +12,24 @@ const CategoryDetails = ({ categoryName }) => {
         }
         getData();
     }, [categoryName])
-    return (
-        <div>{JSON.stringify(categoryDetails)}</div>
-    )
+    return <>
+        {
+            categoryDetails ?
+                <div className=' grid grid-cols-1'>
+                    <div className=' m-2 p-2 w-64 grid grid-cols-1 text-left rounded-lg bg-slate-200 gap-4'>
+                        <div className=' capitalize'><span className=' text-slate-500'>Category: </span>  {categoryDetails.name}</div>
+                        <div> <span className=' text-slate-500'>Description: </span> {categoryDetails.descriptions[0].description} </div>
+                    </div>
+                    <div className=' grid grid-cols-3 md:grid-cols-5 lg:grid-cols-9 gap-4 m-2 p-2 rounded-lg bg-slate-200'>
+                        {
+                            categoryDetails.moves.map((e, i) => <Link key={i} className=' capitalize hover:text-blue-500 hover:cursor-pointer hover:underline rounded-lg bg-slate-100 p-2 m-2' to={`/t-pokedex/move/${e.name}`}> {e.name}</Link>)
+                        }
+                    </div>
+                </div>
+                :
+                <LoadingSpinner />
+        }
+    </>
 }
 
 export default CategoryDetails
