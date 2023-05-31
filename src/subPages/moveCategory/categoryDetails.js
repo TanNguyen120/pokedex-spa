d9334b7bb2fda91487c8622c4a8f4a4fc49aed17
Tan Nguyen <tannguyen1220@gmail.com>
2023-05-31 07:12:17 +0700

    ui optimize

--- src/subPages/moveCategory/categoryDetails.js
+++ src/subPages/moveCategory/categoryDetails.js
@@ -15,14 +15,14 @@ const CategoryDetails = ({ categoryName }) => {
     return <>
         {
             categoryDetails ?
-                <div className=' grid grid-cols-1'>
-                    <div className=' m-2 p-2 w-64 grid grid-cols-1 text-left rounded-lg bg-slate-200 gap-4'>
+                <div className=' grid grid-cols-1 bg-slate-200 rounded-lg m-2'>
+                    <div className=' m-2 p-2 w-64 grid grid-cols-1 text-left rounded-lg bg-slate-50 gap-4'>
                         <div className=' capitalize'><span className=' text-slate-500'>Category: </span>  {categoryDetails.name}</div>
                         <div> <span className=' text-slate-500'>Description: </span> {categoryDetails.descriptions[0].description} </div>
                     </div>
-                    <div className=' grid grid-cols-3 md:grid-cols-5 lg:grid-cols-9 gap-4 m-2 p-2 rounded-lg bg-slate-200'>
+                    <div className=' grid grid-cols-3 md:grid-cols-5 lg:grid-cols-9 gap-4 m-2 p-2 rounded-lg bg-slate-100'>
                         {
-                            categoryDetails.moves.map((e, i) => <Link key={i} className=' capitalize hover:text-blue-500 hover:cursor-pointer hover:underline rounded-lg bg-slate-100 p-2 m-2' to={`/t-pokedex/move/${e.name}`}> {e.name}</Link>)
+                            categoryDetails.moves.map((e, i) => <Link key={i} className=' capitalize hover:bg-slate-200 hover:text-blue-500 hover:cursor-pointer hover:underline rounded-lg bg-white p-2 m-2' to={`/t-pokedex/move/${e.name}`}> {e.name}</Link>)
                         }
                     </div>
                 </div>
