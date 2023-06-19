a3a181d0ecc4221a228bd1e95acfa918e4c79f06
Taan <tannguyen1220@gmail.com>
2023-06-19 12:31:59 +0700

    learn by poke list

--- src/subPages/pokemonAbilityDetails/learnByPokemon.js
+++ src/subPages/pokemonAbilityDetails/learnByPokemon.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
+import { findPokeByName } from '../../reduxSlicer/findPokeInfoFlag';
 
 const LearnByPokemon = ({ pokeList }) => {
-
+    const pictureMode = useSelector((state) => state.webSettings.pictureMode);
+    const dispatch = useDispatch();
+    const setPokeFlag = (pokeName) => {
+        dispatch(findPokeByName(pokeName))
+    }
     return (
         <div className=' rounded-lg bg-slate-200 p-4 m-3 grid grid-cols-1'>{
             <table class="table-fixed border-collapse border border-slate-400">
@@ -17,8 +23,18 @@ const LearnByPokemon = ({ pokeList }) => {
                 <tbody>
                     {
                         pokeList.map((e, i) => <tr key={i} className='border border-slate-400 odd:bg-white even:bg-slate-100 text-left'>
-                            <td className={' capitalize p-3'}><Link to={`/t-pokedex/version-group/${e.version_group.name}`} className=' hover:cursor-pointer hover:underline hover:text-blue-500'>{e.version_group.name}</Link></td>
-                            <td className=' pl-5 p-3'>{e.flavor_text}</td>
+                            <td onClick={e => setPokeFlag(e.pokemon.name)} className={' capitalize p-3'}><Link to={`/t-pokedex/pokemon/${e.pokemon.name}`} className=' hover:cursor-pointer hover:underline hover:text-blue-500'>{e.pokemon.name}</Link></td>
+                            <td className=' pl-5 p-3' onClick={e => setPokeFlag(e.pokemon.name)}>
+                                <Link to={`/t-pokedex/pokemon/${e.pokemon.name}`} className=' text-center'>
+                                    <img class=' w-12 h-12' src={pictureMode === 'sprite' ? `https://img.pokemondb.net/sprites/sword-shield/icon/${e.pokemon.name}.png` : `https://img.pokemondb.net/artwork/large/${e.pokemon.name}.jpg`} alt={e.pokemon.name} className='h-24 w-28 ' />
+                                </Link>
+                            </td>
+                            <td className=''>
+                                {e.is_hidden ? 'yes' : 'no'}
+                            </td>
+                            <td>
+                                {e.slot}
+                            </td>
                         </tr>)
                     }
 
