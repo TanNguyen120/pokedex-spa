5e0f4a1785ffff20696e9dbf0855c2af03d6fc60
Tan Nguyen <tannguyen1220@gmail.com>
2023-08-12 13:53:51 +0700

    exp table

--- src/subPages/growthRate/growthDropDown.js
+++ src/subPages/growthRate/growthDropDown.js
@@ -17,7 +17,26 @@ const GrowthDropDown = ({ url, name }) => {
                 <summary className="m-1 btn hover:cursor-pointer">{name}</summary>
                 <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-3/4">
                     <li><span title='The formula used to calculate the rate at which the Pokémon species gains level.'>Formula: </span>{growthData.formula}</li>
+                    <li>
+                        <table class="table-auto">
+                            <thead>
+                                <tr>
+                                    <th>Level</th>
+                                    <th>EXP need</th>
 
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                                    <td>Malcolm Lockyer</td>
+                                </tr>
+                                {
+                                    growthData.levels.map((e, i) => <tr key={i} ><th>e.level</th><th>{e.experience}</th></tr>)
+                                }
+                            </tbody>
+                        </table>
+                    </li>
                 </ul>
             </details>
             {/* <div classNameName=' rounded-lg bg-slate-50 hidden'>{JSON.stringify(growthData)}</div> */}
