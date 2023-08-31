091152944bf129c473341a389d732e766195f0f9
Tan Nguyen <tannguyen1220@gmail.com>
2023-08-31 10:20:54 +0700

    add more type details

--- src/subPages/pokemonTypes/typeDetails.js
+++ src/subPages/pokemonTypes/typeDetails.js
@@ -97,8 +97,37 @@ const TypeContent = ({ typeDetails }) => {
                 </div>
             </div>
         </div>
-        <div className=' grid grid-cols-1 bg-slate-200 rounded-lg p-2'>
-            <div className=' text-slate-400 text-2xl text-left ml-5'>Damage Relations</div>
+        <div className=' grid grid-cols-1 bg-slate-200 rounded-lg p-2 my-2'>
+            <div className=' text-slate-400 text-2xl text-left ml-5'>Generation Indices</div>
+            <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4   p-8 rounded-lg bg-slate-100'>
+                <div className=' flex flex-row rounded-lg bg-slate-100  m-2 items-baseline'>
+                    <div className=' text-lg text-slate-500'>Debut Gen: </div>
+                    <Link className=' ml-4 capitalize ' to={`/t-pokedex/generation/${typeDetails.generation.name}`}>{typeDetails.generation.name}</Link>
+                </div>
+                <div className=' flex flex-row rounded-lg bg-slate-100  m-2 items-baseline'>
+                    <div className=' text-lg text-slate-500'>Appears in: </div>
+                    <div className=' grid grid-cols-1 gap-3'>
+                        {
+                            typeDetails.game_indices.map((e, i) => <Link className=' hover:bg-slate-300 ml-4 capitalize ' to={`/t-pokedex/generations/${e.generation.name}`}>{e.generation.name}</Link>)
+                        }
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div className=' grid grid-cols-1 bg-slate-200 rounded-lg p-2 my-2'>
+            <div className=' text-slate-400 text-2xl text-left ml-5'>Move type</div>
+            <div className=' grid grid-cols-1  p-8 rounded-lg bg-slate-100'>
+                <div className=' flex flex-row rounded-lg bg-slate-100  m-2 items-baseline'>
+                    <div className=' text-lg text-slate-500'>Damage Class: </div>
+                    <Link className=' ml-4 capitalize ' to={`/t-pokedex/move-damage-class/`}>{typeDetails.move_damage_class.name}</Link>
+                </div>
+                <div className=' grid grid-cols-3 md:grid-cols-5 lg:grid-cols-8 gap-4'>
+                    {
+                        typeDetails.moves.map((e, i) => <Link className='text-left bg-slate-50 rounded-lg p-2 hover:bg-slate-200 hover:scale-110 hover:cursor-pointer' key={i} to={`/t-pokedex/move/${e.name}`}>{e.name}</Link>)
+                    }
+                </div>
+            </div>
         </div>
     </div>
 }
